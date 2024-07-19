0e1025d8d119171db37ed90415796ab9d27428f3
Anna Hartung <annamailda@gmail.com>
2024-07-19 17:42:48 +0800

    pokemons
    pokemons

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
-alert('Hello world');
-
-
-let faviriteFood = 'Salmon';
-document.write(faviriteFood);
-
-faviriteFood = 'Apple';
-document.write(faviriteFood);
+let pokemonList = [
+    { name: 'Caterpie', height: 6, types:  ['monster', 'dragon']},
+    { name: 'Charmeleon', height: 2, types:  ['grass', 'monster']},
+    { name: 'Venusaur', height: 3, types:  ['bug', 'poison']}
+  ];
+   
+  
+  console.log(pokemonList[1]);

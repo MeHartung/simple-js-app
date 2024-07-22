512d88a66159789016b96f84daea697f119f0fdf
Anna Hartung <annamailda@gmail.com>
2024-07-22 13:48:24 +0800

    updates
    updates

--- scripts.js
+++ scripts.js
@@ -1,8 +1,15 @@
 let pokemonList = [
-    { name: 'Caterpie', height: 6, types:  ['monster', 'dragon']},
-    { name: 'Charmeleon', height: 2, types:  ['grass', 'monster']},
-    { name: 'Venusaur', height: 3, types:  ['bug', 'poison']}
-  ];
-   
-  
-  console.log(pokemonList[1]);
+  { name: 'Caterpie', height: 6, types: ['monster', 'dragon'] },
+  { name: 'Charmeleon', height: 2, types: ['grass', 'monster'] },
+  { name: 'Venusaur', height: 3, types: ['bug', 'poison'] }
+];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height < 3) {
+    document.write(pokemonList[i].name + " - is a baby<br>");
+  } else if (pokemonList[i].height < 5 && pokemonList[i].height > 3) {
+    document.write(pokemonList[i].name + " - no worries<br>");
+  } else {
+    document.write(pokemonList[i].name + " - Wow, be careful<br>");
+  }
+}

c9a251aed0f17c269ee07e139b3a8ca22258f135
Anna Hartung <annamailda@gmail.com>
2024-07-23 15:33:23 +0800

    for each
    for each

--- scripts.js
+++ scripts.js
@@ -4,12 +4,14 @@ let pokemonList = [
   { name: 'Venusaur', height: 3, types: ['bug', 'poison'] }
 ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height < 3) {
-    document.write(pokemonList[i].name + " - is a baby<br>");
-  } else if (pokemonList[i].height < 5 && pokemonList[i].height > 3) {
-    document.write(pokemonList[i].name + " - no worries<br>");
+function myLoopFunction(pokemon) {
+  if (pokemon.height < 3) {
+    document.write(pokemon.name + " - is a baby<br>");
+  } else if (pokemon.height <= 5 && pokemon.height >= 3) {
+    document.write(pokemon.name + " - no worries<br>");
   } else {
-    document.write(pokemonList[i].name + " - Wow, be careful<br>");
+    document.write(pokemon.name + " - Wow, be careful<br>");
   }
 }
+
+pokemonList.forEach(myLoopFunction);
